507bd519afea872ba496a7a4d2225734f99fdfa2
HunterG333 <huntergreer03@gmail.com>
2024-07-31 11:29:01 -0700

    Removed unneeded imports

--- src/main/java/com/Greer/Financial_Tracker/model/PersonEntity.java
+++ src/main/java/com/Greer/Financial_Tracker/model/PersonEntity.java
@@ -5,12 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
-import java.util.Collection;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
